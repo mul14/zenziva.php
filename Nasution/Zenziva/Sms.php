e8431c7334f66ba3f101d86727b234e1a9543218
Mulia Arifandi Nasution <mul14.net@gmail.com>
2015-04-16 18:04:15 +0700

    Initialize

---
+++ Nasution/Zenziva/Sms.php
@@ -0,0 +1,82 @@
+<?php namespace Nasution\Zenziva;
+
+use Curl\Curl;
+
+class Zenziva
+{
+    protected $baseURL = 'https://reguler.zenziva.net/apps/';
+
+    protected $userkey, $passkey;
+
+    public function __construct($userkey = null, $passkey = null)
+    {
+        if (!empty($credentials)) {
+            $this->setup($userkey, $passkey);
+        }
+    }
+
+    public function setup($userkey, $passkey)
+    {
+        $this->userkey = $userkey;
+        $this->passkey = $passkey;
+
+    }
+
+    /**
+     * Send to somewhere
+     * @param string $target
+     * @param array $payload
+     * @return null
+     */
+    protected static function send($credentials, $target, array $payload = [])
+    {
+        $curl = new Curl();
+        $curl->setOpt(CURLOPT_RETURNTRANSFER, true);
+        $curl->setOpt(CURLOPT_SSL_VERIFYPEER, false);
+        $curl->setOpt(CURLOPT_SSL_VERIFYHOST, false);
+
+        $credentials = array(
+            'userkey' => $credentials['userkey'],
+            'passkey' => $credentials['passkey'],
+        );
+
+        $query = '?' . http_build_query($credentials);
+
+        $obj = new self::__constru
+
+
+
+        $url = $this->baseURL . $target . $query;
+
+        $curl->get($url);
+
+        return $curl->response;
+    }
+
+    /**
+     * Send SMS
+     * @param string $phoneNumber Phone number
+     * @param string $message Message to send
+     */
+    public static function sendSMS($phoneNumber, $message)
+    {
+        $data['nohp'] = $phoneNumber;
+        $data['pesan'] = $message;
+
+        $data = array(
+            'nohp' => $phoneNumber,
+            'pesan' => $message
+        );
+
+        return static::send('smsapi.php', $data);
+    }
+
+    /**
+     * Check credit
+     */
+    public static function checkCredit()
+    {
+        return static::send('smsapibalance.php');
+    }
+
+}
